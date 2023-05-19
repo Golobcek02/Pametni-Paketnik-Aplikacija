7aca9e87ab985be2b294a992c09a1ead7fb96477
ReneRajzman <rene.rajzman@student.um.si>
2023-05-19 16:14:17 +0200

    Api call sucessful, issue with media player

--- app/src/main/java/com/example/pametnipaketnik/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/pametnipaketnik/ui/notifications/NotificationsFragment.kt
@@ -3,7 +3,9 @@ package com.example.pametnipaketnik.ui.notifications
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +16,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.pametnipaketnik.databinding.FragmentNotificationsBinding
+import com.google.gson.annotations.SerializedName
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import com.journeyapps.barcodescanner.CaptureActivity
@@ -26,7 +29,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.concurrent.TimeUnit
+import java.util.zip.ZipInputStream
 
 class PortraitCaptureActivity : CaptureActivity() {
     // No need to override anything here
@@ -45,7 +53,10 @@ data class OpenBoxRequest(
     val addAccessLog: Boolean
 )
 
-data class OpenBoxResponse(val tokenData: String)
+data class OpenBoxResponse(
+    @SerializedName("data") val tokenData: String
+)
+
 
 interface Direct4meApi {
     @Headers("Content-Type: application/json", "Authorization: Bearer 9ea96945-3a37-4638-a5d4-22e89fbc998f")
@@ -59,6 +70,7 @@ class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private var mediaPlayer: MediaPlayer? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -136,8 +148,8 @@ class NotificationsFragment : Fragment() {
 
         val openBoxRequest = OpenBoxRequest(
             deliveryId = 0,
-            boxId = boxId.toInt(),
-            tokenFormat = 5,
+            boxId = 538,
+            tokenFormat = 2,
             latitude = 0.0,
             longitude = 0.0,
             qrCodeInfo = "string",
@@ -151,21 +163,77 @@ class NotificationsFragment : Fragment() {
         val openBoxCall = service.openBox(openBoxRequest)
         println("cetrta")
 
+
+
         openBoxCall.enqueue(object: Callback<OpenBoxResponse> {
             override fun onResponse(call: Call<OpenBoxResponse>, response: Response<OpenBoxResponse>) {
-                println("peta")
-                println(response)
                 if (response.isSuccessful) {
-                    println("sesta")
                     val tokenData = response.body()?.tokenData
-                    println("API KLIC")
-                    val qrText: TextView = binding.tokenText
-                    qrText.text = tokenData
-                    //playToken(tokenData)
+                    if (tokenData != null) {
+                        println("Token data: $tokenData")
+                        val encodedString = tokenData
+                        val decodedBytes = Base64.decode(encodedString, Base64.DEFAULT)
+                        var file : File?=null
+                        try {
+                            file = File(context?.cacheDir, "audio.mp3")
+                            val fos = FileOutputStream(file)
+                            fos.write(decodedBytes)
+                            fos.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+
+                        val mediaPlayer = MediaPlayer()
+                        try {
+                            if (file != null) {
+                                mediaPlayer.setDataSource(file.path)
+
+                            }
+                            mediaPlayer.prepare()
+                            mediaPlayer.start()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+
+                        /*val byteArray = Base64.decode(tokenData, Base64.DEFAULT)
+                        val filePath = context?.filesDir?.absolutePath + "/token.mp3"
+                        try {
+                            val fos = FileOutputStream(filePath)
+                            fos.write(byteArray)
+                            fos.close()
+                            println("filesize: "+ filePath)
+                            mediaPlayer = MediaPlayer().apply {
+                                setDataSource(filePath)
+                                prepare()
+                                start()
+                            }
+
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }*/
+
+                    } else {
+                        println("tokenData is null")
+                    }
                 } else {
                     println("neuspesen response")
                     println("Response code: " + response.code())
                     println("Error body: " + response.errorBody()?.string())
+                }
+            }
+
+            private fun showPlayButton(filePath: String) {
+                binding.playButton.visibility = View.VISIBLE
+                binding.playButton.setOnClickListener {
+                    mediaPlayer = MediaPlayer()
+                    try {
+                        mediaPlayer!!.setDataSource(filePath)
+                        //mediaPlayer!!.prepare()
+                        mediaPlayer!!.start()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
                 }
             }
 
