5d159be98685a4e492755e193c56208b17fc3078
ReneRajzman <rene.rajzman@student.um.si>
2023-05-23 20:46:10 +0200

    added styling to recycleview

--- app/src/main/java/com/example/pametnipaketnik/recyclerView/OrderAdapter.kt
+++ app/src/main/java/com/example/pametnipaketnik/recyclerView/OrderAdapter.kt
@@ -1,6 +1,7 @@
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pametnipaketnik.API.GetUserOrders.Order
@@ -9,9 +10,9 @@ import com.example.pametnipaketnik.R
 class OrderAdapter(private val orders: List<Order>) : RecyclerView.Adapter<OrderAdapter.OrderViewHolder>() {
 
     class OrderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val idTextView: TextView = view.findViewById(R.id.id_text_view)
         val boxIdTextView: TextView = view.findViewById(R.id.boxId_text_view)
-        // Add more views based on your item_order layout
+        val statusTextView: TextView = view.findViewById(R.id.status_text_view)
+        val itemsLinearLayout: LinearLayout = view.findViewById(R.id.items_linear_layout)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
@@ -21,9 +22,24 @@ class OrderAdapter(private val orders: List<Order>) : RecyclerView.Adapter<Order
 
     override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
         val order = orders[position]
-        holder.idTextView.text = order.id
-        holder.boxIdTextView.text = order.boxId.toString()
-        // Bind more data to views
+        holder.boxIdTextView.text ="Box ID:  ${order.boxId.toString()}"
+        holder.statusTextView.text ="Status:  ${order.status}"
+        // Add the items to the LinearLayout
+        holder.itemsLinearLayout.removeAllViews() // Clear any old views
+        order.items?.forEach { item ->
+            val itemTextView = TextView(holder.itemView.context)
+            itemTextView.text = item
+
+            // Set a top margin for spacing between items
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(0, 8, 0, 0) // Top margin is 8dp
+            itemTextView.layoutParams = layoutParams
+
+            holder.itemsLinearLayout.addView(itemTextView)
+        }
     }
 
     override fun getItemCount() = orders.size
