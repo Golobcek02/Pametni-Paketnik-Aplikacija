71da33787ca2ac466b6cd09d65b9a2a118d0dd7c
PainOfExistance <matej113a@gmail.com>
2024-01-16 20:31:40 +0100

    changed tsp test

--- app/src/main/java/com/example/pametnipaketnik/TSP/TSPtest.kt
+++ app/src/main/java/com/example/pametnipaketnik/TSP/TSPtest.kt
@@ -4,35 +4,11 @@ import com.example.pametnipaketnik.TSP.RandomUtils
 import java.io.File
 
 
+
 object TSPTest {
     @JvmStatic
     fun main(args: Array<String>) {
         RandomUtils.setSeedFromTime() // nastavi novo seme ob vsakem zagonu main metode (vsak zagon bo drugačen)
-
-        // primer zagona za problem eil101.tsp
-        /*var stats = Statistics()
-        var list = mutableListOf<Double>()
-        for (i in 0..29) {
-            val eilTsp = TSP("src/main/resources/bays29.tsp", 1000000)
-            //val eilTsp = TSP("src/main/resources/bays29.tsp", 10000)
-            val ga = GA(100, 0.8, 0.1)
-            val bestPath: Tour = ga.execute(eilTsp)
-            //println(bestPath.distance)
-            stats.add("neke",bestPath.distance)
-            list.add(bestPath.distance)
-            // shrani min, avg in std
-        }
-        println(RandomUtils.getSeed()) // izpiše seme s katerim lahko ponovimo zagon
-
-        stats.dictionary.forEach() {
-            println("${it.key} ${it.value.forEach { i-> println(i) }}")
-        }
-        println()
-        println("-------------------")
-        list.forEach { println(it) }
-        println("-------------------")
-        println(stats.returnStatisticsForProblem("neke"))*/
-
         var problems = listOf(
             Pair("src/main/resources/bays29.tsp", 1000),
             Pair("src/main/resources/eil101.tsp", 1000),
@@ -45,7 +21,7 @@ object TSPTest {
 
         for (i in 0..3) {
             when (i) {
-                /*1 -> {
+                1 -> {
                     problems = listOf(
                         Pair("src/main/resources/bays29.tsp", 10000),
                         Pair("src/main/resources/eil101.tsp", 10000),
@@ -66,7 +42,7 @@ object TSPTest {
                     )
                     stats = Statistics()
                     file = File("fes_100000.txt")
-                }*/
+                }
                 3 -> {
                     problems = listOf(
                         Pair("src/main/resources/bays29.tsp", 1000000),
@@ -80,12 +56,9 @@ object TSPTest {
                 }
             }
 
-            //TODO FIXNI TO POL
-
-
             for (problem in problems) {
                 println(problem.first)
-                for (i in 0 until 29) {
+                for (i in 0..29) {
                     val tsp= TSP(problem.first, problem.second)
                     val ga = GA(100, 0.8, 0.1)
                     val solution = ga.execute(tsp)
